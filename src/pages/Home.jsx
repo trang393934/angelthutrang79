61f90b9e1a9a63d0f8914056204f983313d28262
trang393934 <trang393934@gmail.com>
2025-12-27 15:08:52 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -460,7 +460,7 @@ export default function Home() {
       </section>
 
       {/* Vision & Mission Section */}
-      <section className="relative py-8 lg:py-10 px-4 lg:px-8 bg-gradient-to-b from-transparent via-amber-50/20 to-transparent">
+      <section className="relative py-4 lg:py-6 px-4 lg:px-8 bg-gradient-to-b from-transparent via-amber-50/20 to-transparent">
         <div className="max-w-6xl mx-auto">
           <motion.div
             initial={{ opacity: 0, y: 30 }}
